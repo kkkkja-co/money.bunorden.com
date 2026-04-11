c9de5f3c2315a1a6166c793eaf698b0552f28383
kkkkja-co <kkkja19@protonmail.com>
2026-04-11 20:25:14 +0800

    UI bugs fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,9 +38,7 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="Clavi" />
-        <link rel="apple-touch-icon" href="/assets/icon-192.png" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link rel="apple-touch-icon" href="/assets/clavi-icon-dark.svg" />
       </head>
       <body>
         <Providers>{children}</Providers>
