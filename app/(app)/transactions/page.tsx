494fe407d67255037a7308e6f8e51ada75812d5f
kkkkja-co <kkkja19@protonmail.com>
2026-04-11 22:57:17 +0800

    Update page.tsx

--- app/(app)/transactions/page.tsx
+++ app/(app)/transactions/page.tsx
@@ -191,7 +191,6 @@ export default function TransactionsPage() {
               </button>
             )}
           </div>
-          </div>
 
           {/* Category Filter */}
           {uniqueCategories.length > 0 && (
